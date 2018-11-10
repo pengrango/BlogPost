55de32275db0c7f2c3c7cef1e262f9c181ac816e
Pengran Zhao <Pengran.Zhao@alten.se>
2018-11-10 15:55:07 +0100

    initialize values for all vehicles

--- src/main/java/com/vehicles/monitor/config/ApplicationConfig.java
+++ src/main/java/com/vehicles/monitor/config/ApplicationConfig.java
@@ -1,21 +1,45 @@
 package com.vehicles.monitor.config;
 
+import com.vehicles.monitor.model.Company;
 import com.vehicles.monitor.model.VehicleInfoDomain;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
+import java.time.LocalDateTime;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
 
 @Configuration
 public class ApplicationConfig {
 
+    @Autowired
+    YAMLConfig ymlConfig;
+
     @Bean
     @Scope("singleton")
     public Map<String, VehicleInfoDomain> allVehicleStatus() {
-        Map<String, VehicleInfoDomain> vehiclesMap = new ConcurrentHashMap<String, VehicleInfoDomain>();
-        return vehiclesMap;
+        return getInitialVehicles();
+    }
+
+    private Map<String, VehicleInfoDomain> getInitialVehicles() {
+        Map<String, VehicleInfoDomain> initialVehiclesMap = new ConcurrentHashMap<String, VehicleInfoDomain>();
+
+        for (Company company : ymlConfig.getCompanies()) {
+            Map<String, VehicleInfoDomain> initialVehiclesPerCompany = getVehiclesByCompany(company);
+            initialVehiclesMap.putAll(initialVehiclesPerCompany);
+
+        }
+        return initialVehiclesMap;
+    }
+
+    private Map<String, VehicleInfoDomain> getVehiclesByCompany(Company company) {
+       return  company.getVehicles().entrySet().stream().collect(Collectors.toMap(
+                e -> e.getKey() + "-" + e.getValue(),
+                e -> new VehicleInfoDomain(e.getKey(), e.getValue(), company.getName(),company.getAddress(), LocalDateTime.MIN)
+        ));
     }
 
 }
